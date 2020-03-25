d2d2ee4490674e7bd07a13b3a7bebc0f3d7f9900
Ramon Thomaz <ramonwirtschaftler@gmail.com>
2020-03-25 17:07:24 -0300

    Read Database Test Successful

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,11 +3,31 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
         Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Statement statement = null;
+        ResultSet resultSet = null;
 
+        try {
+            statement = conn.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM department");
+            while (resultSet.next()){
+                System.out.println("Department ID: " + resultSet.getInt("Id") + "; Department Name: " + resultSet.getString("Name"));
+            }
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(resultSet);
+            DB.closeStatement(statement);
+            DB.closeConnection();
+        }
     }
 }
